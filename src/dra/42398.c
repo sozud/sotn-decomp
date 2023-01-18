597b3c0c374beae33d7f11d82bd36d9023bc7af1
sozud <122322823+sozud@users.noreply.github.com>
2023-01-17 21:45:49 -0800

    Decompile func_800FD7C0

--- src/dra/42398.c
+++ src/dra/42398.c
@@ -3001,7 +3001,26 @@ s32 func_800FD77C(s32 context, s32 arg1) {
     return *(&D_800A7718 + (arg1 << 3));
 }
 
-INCLUDE_ASM("asm/dra/nonmatchings/42398", func_800FD7C0);
+u32 func_800FD7C0(u32 itemNum, u32 compareType) {
+    u32 var_v0;
+
+    var_v0 = compareType < 5;
+    if (var_v0 != false) {
+        switch (compareType) {
+        case 0:
+            return (player_equip_head[0] == itemNum) + (player_equip_body == itemNum);
+        case 1:
+            return player_equip_cloak == itemNum;
+        case 2:
+            return player_equip_ring1 == itemNum;
+        case 3:
+            return player_equip_ring2 == itemNum;
+        case 4:
+            return (D_80097C14 == itemNum) + (D_80097C18 == itemNum);
+        }
+    }
+    // seems to require missing return
+}
 
 #ifndef NON_EQUIVALENT
 INCLUDE_ASM("asm/dra/nonmatchings/42398", func_800FD874);
