1d8983849a344da875aa6cac2727ee77f5266f17
sozud <122322823+sozud@users.noreply.github.com>
2023-08-09 19:46:49 -0700

    linker

--- test/one_file/build.sh
+++ test/one_file/build.sh
@@ -2,4 +2,5 @@ mkdir -p out
 ../../bin/cc1-psx-26 -O2 -mcpu=3000 -fgnu-linker -mgas -gcoff all.c -o ./out/all.s
 python3 ../../tools/maspsx/maspsx.py --no-macro-inc --expand-div ./out/all.s > ./out/all_m.s
 mipsel-linux-gnu-as ./out/all_m.s -o ./out/all.o
-mipsel-linux-gnu-ld -Map ./out/one_file.map -o ./out/one_file.elf ./out/all.o 
+mipsel-linux-gnu-ld -T all.ld -Map ./out/one_file.map -o ./out/one_file.elf ./out/all.o 
+mipsel-linux-gnu-objdump -M -march=mips3000 -D  ./out/one_file.elf > ./out/one_file.s
