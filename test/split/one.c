c138d6910cf465a45b7421b63af9304ae9d63882
sozud <sozud@users.noreply.github.com>
2023-09-03 23:14:14 -0700

    updates

--- test/split/one.c
+++ test/split/one.c
@@ -1,10 +1,1 @@
-const int rodata_0[4] = {0,1,2,3};
-
-int data_0[4] = {1,2,3,4};
-
-
-int func_a()
-{
-    data_0[0] = 2;
-    return 0;
-}
+#include "../one.h"
---
+++ test/three.h
@@ -0,0 +1,19 @@
+
+const int rodata_3[4] = {0,1,2,3};
+
+int data_3[4] = {1,2,3,4};
+
+int bss_2;
+
+int _main()
+{
+    func_a();
+    func_b();
+    data_3[0] = 2;
+    return 0;
+}
+
+int __start()
+{
+    _main();
+}
