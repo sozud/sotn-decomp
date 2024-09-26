53a6ac91d09cb2bbaa5917d4acca102dd24b7f98
sozud <122322823+sozud@users.noreply.github.com>
2024-09-26 14:19:19 -0700

    updates

--- src/servant/tt_001/F84.c
+++ src/servant/tt_001/F84.c
@@ -47,17 +47,54 @@ void func_us_801728F4(void);
 void func_us_801728FC(void);
 void DestroyServantEntity(Entity* self);
 
+
+
+// glabel D_us_80170000
+// /* 0 80170000 24161780 */ .word func_us_80171624
+// /* 4 80170004 64181780 */ .word func_us_80171864
+// /* 8 80170008 A4201780 */ .word func_us_801720A4
+// /* C 8017000C AC201780 */ .word func_us_801720AC
+// /* 10 80170010 B4201780 */ .word func_us_801720B4
+// /* 14 80170014 BC201780 */ .word func_us_801720BC
+// /* 18 80170018 C4201780 */ .word func_us_801720C4
+// /* 1C 8017001C CC201780 */ .word func_us_801720CC
+// /* 20 80170020 D4201780 */ .word func_us_801720D4
+// /* 24 80170024 DC201780 */ .word func_us_801720DC
+// .size D_us_80170000, . - D_us_80170000
+
+// glabel D_us_80170028
+// /* 28 80170028 E4201780 */ .word func_us_801720E4
+// .size D_us_80170028, . - D_us_80170028
+
+// glabel D_us_8017002C
+// /* 2C 8017002C 6C241780 */ .word func_us_8017246C
+// /* 30 80170030 EC281780 */ .word func_us_801728EC
+// /* 34 80170034 F4281780 */ .word func_us_801728F4
+// /* 38 80170038 FC281780 */ .word func_us_801728FC
+// /* 3C 8017003C 04291780 */ .word DestroyServantEntity
+// .size D_us_8017002C, . - D_us_8017002C
+
+// glabel D_us_80170040
+// /* 40 80170040 00000000 */ .word 0x00000000
+
+
 ServantDesc_Brk1 g_ServantDesc_Brk1 = {
     func_us_80171624, func_us_80171864, func_us_801720A4, func_us_801720AC,
     func_us_801720B4, func_us_801720BC, func_us_801720C4, func_us_801720CC,
-    func_us_801720D4, func_us_801720DC};
+    func_us_801720D4, func_us_801720DC, func_us_801720E4, func_us_8017246C,
+        func_us_801728EC, func_us_801728F4, func_us_801728FC,
+    DestroyServantEntity
+    };
 
-void (*Unk28)(Entity* self) = &func_us_801720E4;
-void (*Unk2C)(/*?*/) = &func_us_8017246C;
 
-ServantDesc_Brk2 g_ServantDesc_Brk2 = {
-    func_us_801728EC, func_us_801728F4, func_us_801728FC,
-    DestroyServantEntity};
+const s32 pad = 0;
+
+// void (*Unk28)(Entity* self) = &func_us_801720E4;
+// void (*Unk2C)(/*?*/) = &func_us_8017246C;
+
+// ServantDesc_Brk2 g_ServantDesc_Brk2 = {
+//     func_us_801728EC, func_us_801728F4, func_us_801728FC,
+//     DestroyServantEntity};
 #endif
 
 INCLUDE_ASM("servant/tt_001/nonmatchings/F84", func_us_80170F84);
